5c74b61d175119e3fb9fbdda04b640d9f4d8c96e
Lerato <leratomshengu88@gmail.com>
2021-11-11 18:05:08 +0200

    lerato first commit

---
+++ Pro3/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pro3.Models
+{
+    public class Person
+    {
+        int id { get; set; }
+        string email { get; set; }
+        string password { get; set; }
+    }
+}
