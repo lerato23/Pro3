5060946a5ae042efed207fe8b4f1764e814d9277
lerato23 <92342798+lerato23@users.noreply.github.com>
2021-11-23 06:11:58 +0200

    Add files via upload

---
+++ Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pro3.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string TitleName { get; set; }
+        public DateTime Created { get; set; }
+
+        public List<string> Tags { get; set; }
+    }
+}
---
+++ AlbumController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Pro3.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private readonly Image _iIMageService;
+
+        public AlbumController(Image iIMageService)
+        {
+            _iIMageService = iIMageService;
+        }
+        // GET: Album
+        public ActionResult Index()
+        {
+            var imageList = iIMageService.GetAll();
+
+            var model = new AlbumIndexModel()
+            {
+                Images = imageList,
+                SearchQuery = ""
+            };
+            return View();
+        }
+    }
+}
